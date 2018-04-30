8033970d6e9fb5d6202c7413956805116285189a
vladislav savko <vskiev@gmail.com>
2018-04-30 11:38:00 +0300

    vskiev

--- production/modules/motd/manifests/init.pp
+++ production/modules/motd/manifests/init.pp
@@ -5,10 +5,11 @@ class motd {
   $os_release = $facts['os']['release']
   $os_uptime = $facts['system_uptime']['uptime']
 
-  if $hostname == 'Node1'
+  if $hostname == 'Node1'{
    File {'/etc/motd': 
      path => '/etc/motd',
      ensure => file,
      content => "\n\n[PUppet Master] ${hostname} ${os_name} ${os_release} ${uptime}\n\n",
      }
+  }
 }
