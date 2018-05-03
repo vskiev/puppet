e141e0a95aeb75f44ad66f8375f96e3f51263957
vladislav savko <vskiev@gmail.com>
2018-05-03 14:53:03 +0300

    adding ntp class

--- production/manifests/site.pp
+++ production/manifests/site.pp
@@ -1,12 +1,23 @@
+node 'node2.puppet.local' {
+#user_password 
+
+}
+
+
+
+
+
 node default {
   
   include 'motd'
 
-  user { 'glscott_user':
-          name       => 'glscott',
-          groups     => 'wheel',
-          managehome => true,
-          password   => '$1$JjT8L6Dp$i8l.1G5NDQ2/1klpYXeDT.',
-          ensure     => present
-      }
+  # user { 'glscott_user':
+  #         name       => 'glscott',
+  #         groups     => 'wheel',
+  #         managehome => true,
+  #         password   => '$1$JjT8L6Dp$i8l.1G5NDQ2/1klpYXeDT.',
+  #         ensure     => present
+  #     }
+
+  class {'ntp' : }
 }
